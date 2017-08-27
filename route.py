6a24b1bd8daa5d455ac8074b9332a992efbcfd68
niole <niolenelson@gmail.com>
2017-08-27 16:21:02 -0700

    Can make CORS req

--- route.py
+++ route.py
@@ -2,19 +2,22 @@
 The api endpoint for getting the top ten
 tea matches for a user query
 """
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import logging
 import json
 import dbConnection
 from scorer import getRankedMatches
 from dbConnection import Tea
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """
-    serve view
-    """
-    return send_file('index.html')
+logging.getLogger('flask_cors').level = logging.DEBUG
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['CORS_ORIGINS'] = ['http://127.0.0.1:8080']
+app.config['CORS_HEADERS'] = ['Content-Type', 'Authorization']
+app.config['CORS_AUTOMATIC_OPTIONS'] = True
 
 @app.route('/match', methods=['POST'])
 def matcher():
